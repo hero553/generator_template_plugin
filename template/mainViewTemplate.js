0c3c32365dfc3f7dbedee31494eb4c307cc505e0
阿阔 <1055610928@qq.com>
2021-04-01 16:05:09 +0800

    小修改

--- template/mainViewTemplate.js
+++ template/mainViewTemplate.js
@@ -29,7 +29,7 @@ class MainViewTemplate {
       if (item.isSearchOptions) {
         return `
             <div class="filter-item">
-              <el-select v-model="listQuery.${item.name}" placeholder="${item.text}" style="width:300px">
+              <el-select v-model="listQuery.${item.name}" placeholder="请选择${item.text}" style="width:300px">
                 <el-option
                   v-for="item in optiosConfig"
                   :key="item.value"
