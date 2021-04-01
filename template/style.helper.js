0987880f7048b0d6ea08455ea5250861f6fc851f
阿阔 <1055610928@qq.com>
2021-04-01 16:40:44 +0800

    样式修改

--- template/style.helper.js
+++ template/style.helper.js
@@ -2,12 +2,11 @@ const style = `
 .filter-container{
   margin-bottom: 35px;
   display: flex;
+  flex-wrap: wrap;
   margin-top: 20px;
   .filter-item{
-      margin-left: 60px;
-      &:first-child{
-        margin-left: 0;
-      }
+      margin-right: 60px;
+      margin-bottom: 20px;
   }
 }
 `
